aaac003a98bb25e9d9f396b03371104e7ef6c9c0
Akorolchuk <andrey.korolchuk@gmail.com>
2019-11-07 14:40:14 +0200

    mymaintf

--- main.tf
+++ main.tf
@@ -1,47 +1,73 @@
-# Terraform state will be stored in S3
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-alex"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
 
-# Use AWS Terraform provider
+data "aws_availability_zones" "available" {}
+
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-2"
+  
 }
 
-# Create EC2 instance
-resource "aws_instance" "default" {
-  ami                    = "${var.ami}"
-  count                  = "${var.count}"
-  key_name               = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
-  source_dest_check      = false
-  instance_type          = "${var.instance_type}"
+terraform {
+    backend "s3" {
+        bucket = "demo-akorolchuk-terraform-state"
+        key = "dev/network/terraform.tfstate"
+        region = "us-east-2"
+    }
+}
 
-  tags {
-    Name = "terraform-default"
+variable "vpc_cidr" {
+    default ="10.0.0.0/16"
+  
+}
+
+variable "public_subnet_cidrs" {
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+  ]
+}
+
+resource "aws_subnet" "main" {
+  count                   = length(var.public_subnet_cidrs)
+  #Елемнт принимает два параметр лист и индекс листа в count.index добавляем индекс
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  vpc_id     = "${aws_vpc.main.id}"
+#   cidr_block = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+#   availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+  tags = {
+    Name = "Main"
   }
 }
 
-# Create Security Group for EC2
-resource "aws_security_group" "default" {
-  name = "terraform-default-sg"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+resource "aws_vpc" "main" {
+    cidr_block = var.vpc_cidr
+    tags = {
+        Name = "My VPC"
+    }
+  
 }
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+  
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+output "vpc_cidr" {
+  value = aws_vpc.main.cidr_block
+}
+
+# output "subnet_id" {
+#   value = aws_subnet.main.id
+# }
+
+output "public_subnet_ids" {
+  value = aws_subnet.main[*].id
+}
+
+
